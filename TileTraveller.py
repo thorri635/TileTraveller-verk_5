a7171c3e9affce0d37dcc445e17c348ddf97312c
thorsteinn <thorsteinn17@ru.is>
2020-09-17 15:25:56 +0000

    third

--- TileTraveller.py
+++ TileTraveller.py
@@ -8,7 +8,7 @@ while location != (3,1):
             location = (1,2)
         else:
             print("Not a valid direction!")
-    elif location == (1,2):
+    if location == (1,2):
         print("You can travel: (N)orth or (E)ast or (S)outh")
         the_way = input("Direction:")
         if the_way == "N" or the_way == "n":
@@ -19,5 +19,57 @@ while location != (3,1):
             location = (1,1)
         else:
             print("Not a valid direction!")
-    elif loaction == (1,3):
+    if location == (1,3):
         print("You can travel: (E)ast or (S)outh")
+        the_way = input("Direction")
+        if the_way == 'E' or the_way =='e':
+            location = (2,3)
+        elif the_way =='S' or the_way == 's':
+            location =(1,2)
+        else:
+            print("Not a valid direction!")
+    if location == (2,3):
+        print("You can travel: (E)ast or (W)est")
+        the_way = input("Direction:")
+        if the_way == 'E' or the_way =='e':
+            location = (3,3)
+        elif the_way =='W' or the_way =='w':
+            location = (1,3)
+        else:
+            print("Not a valid direction!")
+    if location == (3,3):
+        print('You can travel: (E)ast or (S)outh')
+        the_way= input('Direction:')
+        if the_way =='E' or the_way =='e':
+            location = (2,3)
+        elif the_way =='S' or the_way =='s':
+            location = (3,2)
+        else:
+            print('Not a valid directon!')
+    if location ==(3,2):
+        print('You can travel: (N)orth or (S)outh')
+        the_way= input('Direction:')
+        if the_way =='N' or the_way =='n':
+            location =(3,3)
+        elif the_way =='S' or the_way =='s':
+            location = (3,1)
+        else:
+            print('Not a valid direction!')
+    if location ==(2,2):
+        print('You can travel: (W)est or (S)outh')
+        the_way = input('Direction:')
+        if the_way =='W' or the_way =='w':
+            location = (1,2)
+        elif the_way =='S' or the_way =='s':
+            location = (2,1)
+        else:
+            print('Not a valid direction!')
+    if location ==(2,1):
+        print('You can travel: (N)orth')
+        if the_way =='N' or the_way =='n':
+            location = (2,2)
+        else:
+            print('Not a valid direction!')    
+
+if location ==(3,1):
+    print('Victory')
